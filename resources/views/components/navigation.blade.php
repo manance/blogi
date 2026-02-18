6f0d1051e5ab59e4950e1270bd0594bae284ecd2
Patriks Pužulis <puzulispatriks@gmail.com>
2026-02-18 15:20:25 +0200

    2. push

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Sākums</a></li>
+            <li><a href="/posts">Blogi</a></li>
+            <li><a href="">Pievienot Blogu</a></li>
+            <li><a href="">Kategorijas</a></li>
+            <li><a href="">Pievienot Kategoriju</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:title>Blogi</x-slot:title>
+    <h1>Šeit ir visi Blogu ieraksti:</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li><a href="">{{$post->content}}</a></li>
+        @endforeach
+    </ul>
+</x-layout>
